b3bd4d6bf54ecb28821756069d8a922a51bc7bea
Donald Johnson <donaldrjohnson66@gmail.com>
2023-02-06 19:44:11 -0600

    finished

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+class Program
+ {
+  static void Main(string[] args)
+  {
+   
+  }
+   public string FizzBuzz(int determine)   
+   {
+     var answer = "";
+
+     if(determine % 3 == 0)
+     {
+      answer = "fizz";
+     }
+
+     if(determine % 5 == 0)
+     {
+      answer += "buzz";
+     }
+
+     return answer;
+    }
+
+  }
+ }
